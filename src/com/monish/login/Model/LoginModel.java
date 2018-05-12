b521173601bccfaa8def11c9b42d7980f60e0b09
Mavneswari Ramasamy <monishguru@gmail.com>
2018-05-12 17:09:52 +0530

    First Commit of training web app

---
+++ src/com/monish/login/Model/LoginModel.java
@@ -0,0 +1,49 @@
+package com.monish.login.Model;
+
+public class LoginModel {
+	
+		
+	private String userName;
+	
+	private String password;
+	
+	private int phoneNumber;
+		
+	private String emailId;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+	
+
+}
---
+++ src/com/monish/login/service/LoginBusinessLogic.java
@@ -0,0 +1,26 @@
+package com.monish.login.service;
+
+import com.monish.login.Model.LoginModel;
+
+public class LoginBusinessLogic {
+	
+	public void validateLogin(LoginModel loginModel) {
+		
+		System.out.println("Entered to Validate Login User id and Password");
+				
+		//Create an Array and Add the User name and pwd to Array
+		
+		//if some one had not tried for each Loop,please taverse array Using For Each
+		//and print the Array element
+		
+		//Write an If condition to Check whether user name String is holding some Data
+		
+		//if username is null jus log it 
+		
+		//else we will call DAO
+		
+	
+		
+	}
+
+}
